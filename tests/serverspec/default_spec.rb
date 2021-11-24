6ca739aed49068bcfe4e12d2f692af13157f1960
Tomoyuki Sakurai <y@trombik.org>
2021-11-24 20:18:25 +0700

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,61 @@
 require "spec_helper"
 require "serverspec"
 
-package = "icingaweb2"
-service = "icingaweb2"
-config  = "/etc/icingaweb2/icingaweb2.conf"
-user    = "icingaweb2"
-group   = "icingaweb2"
-ports   = [PORTS]
-log_dir = "/var/log/icingaweb2"
-db_dir  = "/var/lib/icingaweb2"
-
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/icingaweb2.conf"
-  db_dir = "/var/db/icingaweb2"
-end
+package = case os[:family]
+          when "freebsd"
+            "icingaweb2-php74"
+          else
+            "icingaweb2"
+          end
+config_dir = case os[:family]
+             when "freebsd"
+               "/usr/local/etc/icingaweb2"
+             else
+               "/etc/icingaweb2"
+             end
+user    = "www"
+group   = "www"
+config_files = %w[
+  authentication.ini
+  config.ini
+  groups.ini
+  resources.ini
+  roles.ini
+  modules/monitoring/backends.ini
+  modules/monitoring/commandtransports.ini
+  modules/monitoring/config.ini
+]
+db_user = "icingaweb"
+db_name = "icingaweb"
+db_password = "password"
+admin_user = "admin"
 
 describe package(package) do
   it { should be_installed }
 end
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("icingaweb2") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/icingaweb2") do
+config_files.each do |f|
+  describe file("#{config_dir}/#{f}") do
     it { should be_file }
+    it { should be_owned_by user }
+    it { should be_grouped_into group }
+    it { should be_mode 640 }
+    its(:content) { should match(/Managed by ansible/) }
   end
 end
 
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
+# is the database empty?
+describe command "env PGPASSWORD=#{db_password} psql --host 127.0.0.1 --user #{db_user} -c 'SELECT * FROM icingaweb_user' #{db_name}" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/name | active | password_hash | ctime | mtime/) }
 end
 
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+# does the admin user exist?
+describe command "env PGPASSWORD=#{db_password} psql --host 127.0.0.1 --user #{db_user} -c '\\x' -c 'SELECT * FROM icingaweb_user' #{db_name}" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/name\s+\|\s+#{admin_user}/) }
+  its(:stdout) { should match(/active\s+\|\s+1/) }
+  its(:stdout) { should match(/password_hash\s+\|\s+\\x[0-9a-z]+/) }
 end
